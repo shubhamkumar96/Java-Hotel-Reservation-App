9093f440e9362bb9558bf1281335418c34af9307
shubhamkumar96 <shubhamkumarranu@gmail.com>
2021-06-17 07:43:09 +0530

    Added 'equals()' & 'hashCode()' methods

--- src/model/Reservation.java
+++ src/model/Reservation.java
@@ -1,11 +1,26 @@
 package model;
 
 import java.util.Date;
+import java.util.Objects;
 
 public class Reservation {
     Customer customer;
     IRoom room;
     Date checkInDate;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Reservation that = (Reservation) o;
+        return Objects.equals(customer, that.customer) && Objects.equals(room, that.room) && Objects.equals(checkInDate, that.checkInDate) && Objects.equals(checkOutDate, that.checkOutDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(customer, room, checkInDate, checkOutDate);
+    }
+
     Date checkOutDate;
 
     public Reservation(Customer customer, IRoom room, Date checkInDate, Date checkOutDate) {
